585dfa3fdddb17db3052bad9b79e204b503338ec
Kushan233 <hi422815@gmail.com>
2024-12-10 11:51:14 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -30,3 +30,4 @@ SEEDER_PASSWORD: process.env.SEEDER_PASSWORD === undefined ? "" : process.env.SE
 BAD_NO_BLOCK: process.env.BAD_NO_BLOCK === undefined ? "false" : process.env.BAD_NO_BLOCK,  
 POSTGRESQL_URL: process.env.POSTGRESQL_URL === undefined ? 'postgres://vajiratech_user:oSIFl2xmSojMZ0rkzdd0g0W6msuVTpNN@dpg-cpd7fjv109ks73e5gtig-a.frankfurt-postgres.render.com/vajiratech' : process.env.POSTGRESQL_URL,
 };
+
